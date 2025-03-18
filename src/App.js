07ac166d3a4702bbdc05b345196092cface72242
Stephen Kalu <stephenukc@gmail.com>
2025-03-18 16:28:41 +0100

    Add semantic tags and meta tags

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Main from "./Main";
+
 function App() {
-  return <div>Home</div>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
