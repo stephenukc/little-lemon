b6bd33dd790d991ce1f134952a57fab3187a8979
Stephen Kalu <stephenukc@gmail.com>
2025-05-28 19:57:22 +0100

    Add booking and menu models to restaurant

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -77,9 +77,16 @@ WSGI_APPLICATION = 'littlelemon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LittleLemon',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
+ }
 }
 
 
@@ -123,7 +130,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
--- littlelemon/restaurant/admin.py
+++ littlelemon/restaurant/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Booking, Menu
 
-# Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('name', 'no_of_guests', 'booking_date')
+    search_fields = ('name',)
+    list_filter = ('booking_date',)
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'inventory')
+    search_fields = ('title',)
+    list_filter = ('price',)
